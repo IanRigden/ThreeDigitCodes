999f3bb7b42a6c3759db2a55bc81e0675b7ae089
Ian Rigden <ianrigden@hotmail.com>
2022-11-17 16:41:25 +0000

    Initial commit

---
+++ src/ThreeDigitCodes.java
@@ -0,0 +1,20 @@
+public class ThreeDigitCodes {
+    public static void main(String[] args) {
+        int total = 0;
+        for(int i = 1; i <= 4; i++)
+        {
+            for(int j = 1; j <= 4; j++)
+            {
+                if(j != i) {
+                    for (int k = 1; k <= 4; k++) {
+                        if(k != j && k != i){
+                            System.out.println(i + "" + j + "" + k);
+                            total += 1;
+                        }
+                    }
+                }
+            }
+        }
+        System.out.println("There were " + total + " unique combinations.");
+    }
+}
